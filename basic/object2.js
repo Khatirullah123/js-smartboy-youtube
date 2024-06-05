7f564a49f411990caeecd6672ccf239ccf354548
Khatirullah123 <86227465+Khatirullah123@users.noreply.github.com>
2024-06-05 04:42:59 +0000

    done

--- basic/object2.js
+++ basic/object2.js
@@ -18,4 +18,32 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname);
+// console.log(regularUser.fullname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a" , 4: "b" }
+
+// const obj3 = {obj1,obj2}
+
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+
+const obj3 = {...obj1,...obj2} 
+// console.log(obj3);
+
+
+const users = [
+    {
+        id:1,
+        email:"khatirhng@gmail.com",
+
+
+    }
+]
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
