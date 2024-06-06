0a17812133828d97b5ce86dfd066a0424479a1f1
Khatirullah123 <86227465+Khatirullah123@users.noreply.github.com>
2024-06-06 12:11:08 +0000

    done

--- basic/array2.js
+++ basic/array2.js
@@ -1,7 +1,7 @@
 const marval_heros =["thor","Ironman","spiderman"]
 const dc_heros = ["superman","flash","batman"]
-// marval_heros.push(dc_heros)
-// console.log(marval_heros);
+marval_heros.push(dc_heros)  //
+console.log(marval_heros);
 
 
 // const allheros = marval_heros.concat(dc_heros)
@@ -14,12 +14,12 @@ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array);
 
-console.log(Array.isArray("Sahil"));
-console.log(Array.from("Sahil"));
-console.log(Array.from({name : "Sahil"}));
+console.log(Array.isArray("Sahil"));  // to cheak is that array
+console.log(Array.from("Sahil"));    // make array 
+// console.log(Array.from({name : "Sahil"}));
 
 let score1=100
 let score2=200
 let score3=300
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
